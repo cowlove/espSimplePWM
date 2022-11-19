53866063067e7d890752f62dcfb2ad87cb84c645
Jim Evans <jim@vheavy.com>
2022-11-18 20:22:30 -0800

    jimlib changes

--- espSimplePWM.ino
+++ espSimplePWM.ino
@@ -7,7 +7,7 @@ AsyncUDP udp;
 JStuff j;
 
 struct {
-	int pwm = 16;
+	int pwm = 17;
 } pins;
 
 
@@ -19,16 +19,17 @@ CLI_VARIABLE_FLOAT(lo, 300);
 CLI_VARIABLE_FLOAT(hi, 1700);
 CLI_VARIABLE_FLOAT(led, 1);
 CLI_VARIABLE_STRING(test, "test");
-
+int cmdCount = 0;
 void setup() {
-	j.mqtt.active = false;
-
 	j.begin();
 	j.cli.on("MINUTE", [](){ minute.alarmNow(); });
 	ledcSetup(0, 50, 16); // channel 1, 50 Hz, 16-bit width
 	ledcAttachPin(pins.pwm, 0);   // GPIO 33 assigned to channel 1
 
 	j.onConn = []{
+		j.cliEcho = false;
+		j.mqtt.active = false;
+
 		if (udp.listen(1234)) { 
 			udp.onPacket([](AsyncUDPPacket packet) {
 				String s = buf2str(packet.data(), packet.length());
@@ -40,12 +41,13 @@ void setup() {
 				if (sscanf(s.c_str(), "set pwm=%d", &c)) {
 					if (c >= lo && c <= hi) {
 						ledcWrite(0, c * 4915 / 1000);
+						pwm = c;
 					}
 				}
-
+				cmdCount++;
 				//j.cli.process(s.c_str());
 				//led = led != 0 ? 0 : 100;
-				OUT(s.c_str());
+				//OUT(s.c_str());
 			});
 		}
 	};
@@ -53,6 +55,9 @@ void setup() {
 
 void loop() {
 	j.run();
+	if (j.hz(1)) { 
+		OUT("%06.1f %04d %04d", millis() / 1000.0, cmdCount, (int)pwm);
+	}
 	delayMicroseconds(10);
 }
 
